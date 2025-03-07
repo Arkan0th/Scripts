46e8a032629c8baa780596535f82a24caeb46aa2
Arkanoth <juttad00@upol.cz>
2025-03-07 18:04:26 +0100

    Taylor series plotter update (error in line 40)

--- Taylor series plotter/Taylor.py
+++ Taylor series plotter/Taylor.py
@@ -17,16 +17,40 @@ def Taylor_sin(x, n):
         hodnoty.append(soucet)
     return hodnoty
 
+def Taylor_sinh(x, n):
+    soucet = 0
+    hodnoty = []
+    for i in range(n + 1):
+        soucet += (1/np.math.factorial(2*i + 1)) * (x**(2*i + 1))
+        hodnoty.append(soucet)
+    return hodnoty
+
+def Taylor_cosh(x, n):
+    soucet = 0
+    hodnoty = []
+    for i in range(n + 1):
+        soucet += (1/np.math.factorial(2*i)) * (x**(2*i))
+        hodnoty.append(soucet)
+    return hodnoty
+
+def Taylor_sqrt_1_minus_x(x, n):
+    soucet = 0
+    hodnoty = []
+    for i in range(n + 1):
+        soucet += ((np.math.factorial(1/2))/((np.math.factorial(i))*(np.math.factorial(1/2 - i))))*((-x)**i)
+        hodnoty.append(soucet)
+    return hodnoty
+
 x_exp = np.linspace(-1.5, 1.5, 100)
 x_sin = np.linspace(-np.pi, np.pi, 100)
-x_sin_inv = np.linspace(-47.5, 47.5, 100)
-x_sin_inv = x_sin_inv[x_sin_inv != 0]  #neděl nulou
-x_sin_div_x = np.linspace(-6, 6, 100)
+x_sinh = np.linspace(-5, 5, 100)
+x_cosh = np.linspace(-5, 5, 100)
+x_sqrt_1_minus_x = np.linspace(-0.5, 1, 100)
 
-#řád
 n_max = 4
 barvy = ['red', 'blue', 'green', 'orange', 'purple', 'brown']
 
+"""
 #e^x
 plt.figure()
 for n in range(n_max + 1):
@@ -78,5 +102,46 @@ plt.title('Taylorova řada pro sin(x)/x')
 plt.legend()
 plt.grid()
 plt.savefig('Taylor_funkce_sin_div_x.png')
+"""
+
+# sinh(x)
+plt.figure()
+for n in range(n_max + 1):
+    hodnoty_taylor = np.array([Taylor_sinh(xi, n)[-1] for xi in x_sinh])
+    plt.plot(x_sinh, hodnoty_taylor, label=f'Taylor sinh {n}. řád', color=barvy[n % len(barvy)], linewidth=2)
+plt.plot(x_sinh, np.sinh(x_sinh), label='sinh(x)', color='black', linewidth=3)
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title('Taylorova řada pro sinh(x)')
+plt.legend()
+plt.grid()
+plt.savefig('Taylor_funkce_sinh.png')
+
+# cosh(x)
+plt.figure()
+for n in range(n_max + 1):
+    hodnoty_taylor = np.array([Taylor_cosh(xi, n)[-1] for xi in x_cosh])
+    plt.plot(x_cosh, hodnoty_taylor, label=f'Taylor cosh {n}. řád', color=barvy[n % len(barvy)], linewidth=2)
+plt.plot(x_cosh, np.cosh(x_cosh), label='cosh(x)', color='black', linewidth=3)
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title('Taylorova řada pro cosh(x)')
+plt.legend()
+plt.grid()
+plt.savefig('Taylor_funkce_cosh.png')
+
+# sqrt(1 - x)
+plt.figure()
+for n in range(n_max + 1):
+    hodnoty_taylor = np.array([Taylor_sqrt_1_minus_x(xi, n)[-1] for xi in x_sqrt_1_minus_x])
+    plt.plot(x_sqrt_1_minus_x, hodnoty_taylor, label=f'Taylor sqrt(1-x) {n}. řád', color=barvy[n % len(barvy)], linewidth=2)
+plt.plot(x_sqrt_1_minus_x, np.sqrt(1 - x_sqrt_1_minus_x), label='sqrt(1-x)', color='black', linewidth=3)
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title('Taylorova řada pro sqrt(1-x)')
+plt.legend()
+plt.grid()
+plt.savefig('Taylor_funkce_sqrt_1_minus_x.png')
 
 plt.show()
+
